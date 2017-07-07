805dc6faed059da40a1fd56dcf1f7ba6b22495e8
fortune chibuike <chibuikefortune@hotmail.com>
2017-07-07 12:07:49 +0100

    new

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -63,6 +63,6 @@ private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def project_params
-    params.require(:project).permit(:date, :title, :content, :image)
+    params.require(:project).permit(:date, :title, :content, :more, :image)
   end
 end
--- db/migrate/20170706230458_add_atrributes_to_projects.rb
+++ db/migrate/20170706230458_add_atrributes_to_projects.rb
@@ -1,5 +1,6 @@
 class AddAtrributesToProjects < ActiveRecord::Migration[5.0]
   def change
     add_column :projects, :user_id, :integer
+    add_column :projects, :more, :text
   end
 end
